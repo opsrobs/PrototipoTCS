65a1e1ccd9be0b5930c914758ab1ee6a33632b68
matheusavizsenac <matheus.aviz@alunos.sc.senac.br>
2023-05-03 21:58:48 -0300

    Adicionando smell para retorno da view

--- view/gpt_view/view.py
+++ view/gpt_view/view.py
@@ -11,32 +11,21 @@ gpt_blueprint = Blueprint('gpt_view', __name__, template_folder="templates")
 gpt = GptController()
 smell = SmellController()
 
-@gpt_blueprint.route("/historias", methods=("GET", "POST"))
+@gpt_blueprint.route("/historias", methods=["GET", "POST"])
 def historias():
-    prompt = ""
     if request.method == "POST":
         prompt = request.form["prompt"]
         response = gpt.text_to_completion(temperature= 1, prompt = prompt, model = "text-davinci-003", max_tokens=256)
-        return redirect(url_for("gpt_view.historias", result=response.choices[0].text))
-    
-    result = request.args.get("result")
-    if result != None:
+        historia_output = response.choices[0].text
         gpt_model = Gpt()
-        gpt_has_smell = GptHasSmell()
-        gpt_model.set_historia_output(result)
+        gpt_model.set_historia_output(historia_output)
         gpt_model.set_historia_input(prompt)
         db.session.add(gpt_model)
         db.session.commit()
-        last_id = gpt_model.id
-        smells = smell.text_to_get_smells(model="gpt-3.5-turbo", prompt=result)
-        print("id smell --> " + smells.choices[0].message['content'])
-        print(''.join(filter(str.isdigit, smells.choices[0].message['content'])))
-        smell_by_id = smell.get_smell_by_id(''.join(filter(str.isdigit, smells.choices[0].message['content'])))
-        gpt_has_smell.set_id_gpt(last_id)
-        gpt_has_smell.set_id_smell(smell_by_id["id"])
-        db.session.add(gpt_has_smell)
-        db.session.commit()
-    return render_template("gpt.html", result=result)
+        smells = smell.text_to_get_smells(model="gpt-3.5-turbo", prompt=historia_output)
+        resultado = {"historia": response.choices[0].text, "smell": smells.choices[0].message['content']}
+        return render_template("gpt.html", result=resultado)
+    return render_template("gpt.html")
 
 
 @gpt_blueprint.route("/gethistorias")
