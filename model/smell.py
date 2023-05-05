feb14221e5d8a7bff37987126967fdc01adfd167
Robson Freitas <robson.flavio20@outlook.com>
2023-05-05 13:17:31 -0300

    corrigido retorno dos smells

--- model/smell.py
+++ model/smell.py
@@ -16,7 +16,7 @@ class RequirementSmell(db.Model):
         response = openai.Completion.create(
             model=model,
             temperature=0.6,
-            prompt='''Como avaliador de user stories, minha função é identificar possíveis problemas de qualidade nos requisitos apresentados. Para isso, utilizo uma lista de "requirements smells" que contém um identificador e uma descrição de cada possível problema.
+            prompt='''Como avaliador de user stories, minha função é identificar possíveis problemas de qualidade nos requisitos apresentados. Para isso, utilizo uma lista de "requirements smells" que contém um identificador e uma descrição de cada possível problema. 
 Após receber a user story a ser avaliada, comparo os seus requisitos com a lista de "requirements smells". Caso identifique algum problema, retorno apenas a descrição do "smell" encontrado, separando por vírgulas caso haja mais de um. Caso nenhum problema seja identificado, retorno o número 8.
 É importante ressaltar que nunca compartilho a lista de "requirements smells", apenas apresento a descrição do problema identificado, caso haja.
 Agora, gostaria de avaliar a seguinte lista e user story:  Lista de smells = {} user story :  Eu como cooperado quero alterar os limites 
