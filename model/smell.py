cbc85b98b911bce87933fc38e8073637590f42fb
Robson Freitas <robson.flavio20@outlook.com>
2023-05-12 16:02:36 -0300

    Ajustado prompt e e body do json smells

--- model/smell.py
+++ model/smell.py
@@ -17,21 +17,15 @@ class RequirementSmell(db.Model):
         response = openai.Completion.create(
             model=model,
             temperature=0.6,
-            #max_tokens: 256,
+            # max_tokens: 256,
             prompt='''Você receberá um conjunto de User Stories (histórias do usuário) e uma lista de "smells" (problemas) comuns encontrados em requisitos de software. Seu objetivo é identificar se cada User Story contém algum "smell" que corresponda à lista fornecida.
-
 Aqui está o formato esperado para a avaliação:
-
 User Story:
 [Descrição da User Story]
-
 Smells Encontrados:
 [Lista de "smells" presentes na User Story, separados por vírgula]
-
 Se você identificar algum "smell" presente na User Story, enumere-o na seção "Smells Encontrados". Caso contrário, deixe a seção "Smells Encontrados" em branco.
-
 Lembre-se de que cada User Story pode conter mais de um "smell", portanto, verifique cuidadosamente cada uma delas.
-
 Aqui está a lista de "smells" a serem avaliados: {}, user story :  Eu como cooperado quero alterar os limites 
 resposta: "Incomplete references" refere-se a referências ou termos não definidos ou incompletos em uma User Story. Isso pode causar confusão e dificuldade na compreensão dos requisitos. Para evitar esse problema, é importante garantir que todas as referências e termos utilizados sejam claramente definidos e compreendidos por todos os membros da equipe de desenvolvimento. Isso pode ser alcançado através de uma comunicação clara e do esclarecimento de dúvidas para garantir um entendimento comum dos requisitos;
 user story : {}
--- view/gpt_view/view.py
+++ view/gpt_view/view.py
@@ -37,8 +37,8 @@ def get_historias():
     for historia in historias:
         historias_json.append({
             'id': historia.id,
-            'User Storie': historia.historia_input,
-            'User Storie padronizada': historia.historia_output,
+            'UserStorie': historia.historia_input,
+            'UserStoriePadronizada': historia.historia_output,
         })
     return jsonify(historias_json)
     return render_template("gethistorias.html", result=historias_json)
