ee30a39ff3ba1899dc1d433710e6ba809da6c6a1
Robson Freitas <robson.flavio20@outlook.com>
2023-05-17 16:40:02 -0300

    ajustes no prompt

--- model/smell.py
+++ model/smell.py
@@ -20,8 +20,8 @@ class RequirementSmell(db.Model):
             max_tokens= 256,
             prompt='''Você receberá um conjunto de User Stories (histórias do usuário) e uma lista de "smells" (problemas) comuns encontrados em requisitos de software. Seu objetivo é identificar se cada User Story contém algum "smell" que corresponda à lista fornecida.
 Aqui está o formato esperado para a avaliação:
-Smells Encontrados:
-[ID do smell + Lista de "smells" presentes na User Story, separados por vírgula + | Justificativa do smells encontrados]
+ID + Smells Encontrados:
+[IDs do smell + Lista de "smells" presentes na User Story, separados por vírgula + | Justificativa do smells encontrados]
 Se você identificar algum "smell" presente na User Story, enumere-o na seção "Smells Encontrados". Caso contrário, deixe a seção "Smells Encontrados" em branco.
 Lembre-se de que cada User Story pode conter mais de um "smell", portanto, verifique cuidadosamente cada uma delas.
 Aqui está a lista de "smells" a serem avaliados: {},
