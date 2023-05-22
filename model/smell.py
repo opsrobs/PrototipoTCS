1b0d3cb30ea0ec26ffc4072f8486bf2723547b8f
Kelvin Melim <kelvinmelim@gmail.com>
2023-05-22 11:01:11 -0300

    correção temporária no método do prompt. o recebimento do parâmetro lista smells estava quebrando o prompt. Alteração no texto do prompt.

--- model/smell.py
+++ model/smell.py
@@ -13,20 +13,42 @@ class RequirementSmell(db.Model):
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     def get_smell(self, text, model, get_smells):
-        print(text)
+        textvar=text
+        prompt_text ='''Histórias de usuários são requisitos que demonstram como um software deve se comportar, os quais são seguidos pelos desenvolvedores de software no momento da construção, 
+        por exemplo: Dado que sou um usuário do site de rede social, desejo poder postar mídias. 
+        Nesse caso, o dono do produto escreveu uma funcionalidade sobre o sistema de rede social que o usuário deve ter, nesse caso, o usuário pode postar uma mídia no sistema. 
+        Um problema que pode ocorrer nas histórias de usuário é a escrita feita de forma que cause má interpretação pelo desenvolvedor do sistema, o qual por sua vez pode acabar criando a funcionalidade de forma errada. 
+        A partir de agora, irei chamar esses problemas de "requiriments smells". 
+        Considere essa seguinte lista de requiriments smells:  
+        (id: 1, nome: gaps factor, detalhe: is characterized by scenarios that have one or more optional actions in the story.), 
+        (id: 2, nome: incomplete references, detalhe: occurs when artifacts such as complementary story content, such as documentation, technical terms or laws, are mentioned, but the references to these contents are not available or incomplete.), 
+        (id: 3, nome: problem orientation, detalhe: refers to the scenario in which a story presents only the problem by specifying it without any solution.), 
+        (id: 4, nome: comparative, superlative, and subjective language, detalhe: present specifications in comparison or subjective interpretations of the users needs.), 
+        (id: 5, nome: ambiguity, detalhe: can arise from the lack of verification of words that are not specific by nature and tend to cause more than one interpretation and make parameterization difficult.), 
+        (id: 6, nome: conflict, detalhe: occurs when two or more requirements cause inconsistencies between them.), 
+        (id: 7, nome: testable, detalhe: is detected in the absence or inaccuracy of the parameters specified in the stories that make it impossible to create metrics or parameters for testing.), 
+        (id: 8, nome: without errors, detalhe: This number indicates that none of the other options were selected.).
+        Repare que existem 8 requiriments smells, para cada um, separado por vírgulas, existe "id" "nome" e "detalhe". 
+        É importante ressaltar que essa lista nunca deve ser repetida, apenas deve-se retornar o Requiriment Smell encontrado, no caso o "id" "nome" e "detalhe". 
+        Caso encontre mais que um, separe-os com um ponto. 
+        O id 8 é utilizando quando nenhum dos 7 primeiros é encontrado. Para facilitar, considere este exemplo "Dado que sou um usuário do site de rede social, desejo poder postar mídias". 
+        Neste caso, percebe-se dois Requiriments Smells, o id 2 e o id 5. 
+        Nesse exemplo, você deveria retornar exatamente a seguinte resposta: 
+        2. Incomplete References. Occurs when artifacts such as complementary story content, such as documentation, technical terms or laws, are mentioned, but the references to these contents are not available or incomplete. 
+        5. Ambiguity. Can arise from the lack of verification of words that are not specific by nature and tend to cause more than one interpretation and make parameterization difficult. 
+        Então como mostrado no exemplo, ao enviar uma história de usuário, a mesma deve ser lida e comparada com a lista para verificar se algum dos requiriments smells é identificado. 
+        Agora que você sabe como fazer, verifique a seguinte História de Usuário: '''+textvar+''' 
+        resposta:'''#.format(str(text)) 
         response = openai.Completion.create(
             model=model,
-            top_p= 0.1,
+            temperature=0.6,
             max_tokens= 256,
-            prompt='''Você receberá um conjunto de User Stories (histórias do usuário) e uma lista de "smells" (problemas) comuns encontrados em requisitos de software. Seu objetivo é identificar se cada User Story contém algum "smell" que corresponda à lista fornecida.
-Aqui está o fosrmato esperado para a avaliação:
-ID + Smells Encontrados:
-[IDs do smell + Lista de "smells" presentes na User Story, separados por vírgula + | Justificativa do smells encontrados]
-Se você identificar algum "smell" presente na User Story, enumere-o na seção "Smells Encontrados". Caso contrário, deixe a seção "Smells Encontrados" em branco.
-Lembre-se de que cada User Story pode conter mais de um "smell", portanto, verifique cuidadosamente cada uma delas.
-Aqui está a lista de "smells" a serem avaliados: {},
-user story : {}
-resposta: '''.format(* get_smells, text))
+            prompt=prompt_text)
+        print('PROMPT: ')
+        print(prompt_text)
+        print('USER STORIE: ')
+        print(text)
+        print('RESPONSE GPT: ')
         print(response)
         return response
 
