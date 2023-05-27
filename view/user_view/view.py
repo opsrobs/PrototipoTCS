0a24be89f2641c8476edabe4552f8e14682b4d57
matheusavizsenac <matheus.aviz@alunos.sc.senac.br>
2023-05-27 18:21:15 -0300

    expandindo caracteres da senha e view de registro

---
+++ view/user_view/view.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify, redirect, render_template, request, url_for
+from model.user import User
+from controller.user_controller.controller import UserController
+from flask_cors import CORS
+from model import db
+
+user_blueprint = Blueprint('user_view', __name__, template_folder="templates")
+CORS(user_blueprint)
+
+
+user_controller = UserController()
+
+@user_blueprint.route("/register", methods=["GET", "POST"])
+def register_user():
+    if request.method == 'POST':
+        user_model = User()
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        user_model.set_email(email)
+        user_model.set_password(senha)
+        user_model.set_nome(nome)
+        db.session.add(user_model)
+        db.session.commit()
+        return render_template('register.html')
+    return render_template('register.html')
